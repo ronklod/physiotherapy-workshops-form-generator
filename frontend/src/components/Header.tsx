deb9798b71ac7b2888265250dd4a2ed40244a643
ronklod <ronklod@gmail.com>
2025-07-02 22:21:18 +0300

    Add favicon and logo images; update Header component layout
    - Introduced favicon.ico and logo.png to the project.
    - Updated Header component to include a logo alongside the title, enhancing the layout with new CSS styles for better alignment and hover effects.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Header.css';
+import logo from '../images/logo.png';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <h1 className="header-title">מחולל טופס התעמלות פיזיותרפיה</h1>
-        
+        <div className="header-title-container">
+          <h1 className="header-title">מחולל טופס התעמלות פיזיותרפיה</h1>
+          <div className="header-logo-container">
+            <img src={logo} alt="Logo" className="header-logo" />
+          </div>
+        </div>
       </div>
     </header>
   );
